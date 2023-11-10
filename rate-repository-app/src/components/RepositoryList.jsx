d1c3b7e99426530960c94614d2f596eded8f0c58
Zarak Ansari <zarak.ansari@yahoo.com>
2023-11-10 15:09:12 +0500

    work in progress 10-5

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,5 +1,5 @@
 import { FlatList, View, StyleSheet } from 'react-native';
-import RepositoryItem from './RepositoryItem';
+import RepositoryItem from './RepositoryItem/RepositoryItem';
 
 const styles = StyleSheet.create({
     separator: {
